d50e97b455b1149b394e0eb11147957ff7e6552d
HankLu <s210215@shsh.tw>
2020-04-15 17:28:55 +0200

    fix space, change hash

--- url/serializers.py
+++ url/serializers.py
@@ -1,16 +1,13 @@
 import base64
 import hashlib
-
+from django.conf import settings
 from rest_framework import serializers
 from url.models import URL, Visitor
 
 
-HASH_SALT = "meow"
-
-
 def shorten_url(id):
     return base64.b64encode(
-        hashlib.md5((str(id) + HASH_SALT).encode('utf-8')).digest(), altchars=b"-_")[:6].decode("utf-8")
+        hashlib.md5((str(id) + settings.HASH_SALT).encode('utf-8')).digest(), altchars=b"-_").decode("utf-8")
 
 
 class URLSerializer(serializers.ModelSerializer):
@@ -26,7 +23,7 @@ class URLSerializer(serializers.ModelSerializer):
             u = URL.objects.create(url=url, user=user)
 
             code_length = 4
-            while URL.objects.filter(short_code=shorten_url(u.id)[:code_length]):
+            while URL.objects.filter(short_code=shorten_url(u.id)[:code_length]).exists():
                 code_length += 1
             short_code = shorten_url(u.id)[:code_length]
 
